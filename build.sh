61b8d926aee2b451bc71909323577d7edffb864d
recharge1855 <recharge1855@gmail.com>
2026-05-21 15:46:51 +0000

    fix: Netlify build script - handle cached flutter_sdk directory

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter_sdk
+# Install Flutter (skip if cached)
+if [ ! -d "flutter_sdk" ]; then
+  git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter_sdk
+fi
+
 export PATH="$PATH:$(pwd)/flutter_sdk/bin"
 
 # Build Flutter web
